dd226c43e7d10558960ff2b7302886e4e479370c
MiguelMottaVilca <46751756+MiguelMottaVilca@users.noreply.github.com>
2020-04-20 16:11:52 -0500

    Create tools.h

---
+++ vigenere/tools.h
@@ -0,0 +1,34 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <iostream>
+
+using namespace std;
+
+int modulo(int num , int d ){
+    int c , r;
+    c = num / d ;
+    r = num - ( c * d );
+    if (r < 0){
+        c -= 1 ; 
+        r = r + d ;
+    }
+    return r ;
+}
+
+int euclides(int num ,int d ){
+    int r = 1;
+
+    while (r > 0){
+        r  = modulo( num , d);
+        num = d ;
+        d = r;
+    }
+    return num ;
+}
+
+// int euclidesExtend(int num , int d){  
+// }
+
+
+#endif //TOOLS_H
