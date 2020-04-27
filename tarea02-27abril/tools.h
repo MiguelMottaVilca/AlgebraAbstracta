f6103bc38784220cd770e9d84116d8b7cd54bdb6
MiguelMottaVilca <46751756+MiguelMottaVilca@users.noreply.github.com>
2020-04-27 16:38:52 -0500

    Update tools.h

--- tarea02-27abril/tools.h
+++ tarea02-27abril/tools.h
@@ -23,6 +23,15 @@ int valorAbsoluto(int a){
     return a;
 }
 
-
+int euclides(int num , int d){
+    int r = modulo(num,d);
+    while(r > 0){
+        cout << "vuelta nuevo" << endl;
+        num = d;
+        d = r;
+        r = modulo(num,d);
+    }
+    return d;
+}
 
 #endif //TOOLS_H
