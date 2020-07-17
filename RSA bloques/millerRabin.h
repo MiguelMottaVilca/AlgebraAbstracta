409073f4e9a90af783d9897975163a16b2768e7e
MiguelMottaVilca <46751756+MiguelMottaVilca@users.noreply.github.com>
2020-07-17 06:59:05 -0500

    Update millerRabin.h

--- RSA bloques/millerRabin.h
+++ RSA bloques/millerRabin.h
@@ -4,19 +4,6 @@
 using namespace std;
 using namespace NTL;
 
-
-ZZ mulmod(ZZ a, ZZ b, ZZ m) {
-   ZZ x {0} ,y = modulo( a , m);
-   while (b > 0) {
-      if (b % 2 == 1) {
-         x = modulo( (x + y) , m) ;
-      }
-      y =  modulo( (y * 2) , m);
-      b /= 2;
-   }
-   return x % m;
-}
-
 bool MillerRabin(ZZ p, int iteration) {
    if (p < 2) 
       return false;
@@ -34,7 +21,7 @@ bool MillerRabin(ZZ p, int iteration) {
         temp = s;
         mod = expModular(a, temp, p);
         while (temp != p - 1 && mod != 1 && mod != p - 1) {
-            mod = mulmod(mod, mod, p) ;
+            mod = expModular(mod, ZZ(2), p);
             temp *= 2; 
         }
         if (mod != p - 1 && temp % 2 == 0) {
