2c6e24543aa4baabffa401212cab3d106c00ee9a
MiguelMottaVilca <46751756+MiguelMottaVilca@users.noreply.github.com>
2020-07-17 06:33:07 -0500

    Update main.cpp

--- RSA bloques/main.cpp
+++ RSA bloques/main.cpp
@@ -1,40 +1,58 @@
 #include <NTL/ZZ.h>
+#include <NTL/vector.h>
 #include <string>
 #include <sstream>
-#include <vector>
+#include <bitset> 
+#include <cstdlib>
+#include <cstdio>
+#include <array>
+
 #include "tools.h"
-// #include "criba.h"
+#include "restoChino.h"
+#include "aleatotios.h"
+#include "RC4.h"
+#include "DES.h"
+#include "millerRabin.h"
 
 
 using namespace std;
 using namespace NTL;
 
-// string abc ="ABCDEFGHIJKLMNOPQRSTUVWXYZ,.-( )abcdefghijklmnopqrstuvwxyz<>*1234567890";
+string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ,.-( )abcdefghijklmnopqrstuvwxyz<>*1234567890";
 //string abc = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ,.";
-//string abc = "abcdefghijklmnopqrstuvwxyz -ABCDEFGHIJKLMNOPQRSTUVWXYZ,.0123456789";
-string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// string abc = "abcdefghijklmnopqrstuvwxyz -ABCDEFGHIJKLMNOPQRSTUVWXYZ,.0123456789";
+// string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 // string abc = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 
-int aux = abc.size() ; 
-ZZ abc_size{aux};
+int abc_size = abc.size() ; 
 
-#include "rsa.h"
+#include "rsaBloques.h"
 
 int main(){
-    
-    long b = 4;
-    RSA receptor(b);
-    RSA emisor(receptor.n,receptor.e);
-    // cout <<"cifrado :" << 
-    emisor.cifrado("HOLA") ;
-    //<< endl;
-    // cout << receptor.descifrado("122482 38146 ") << endl;
 
-    // string a = "1";
-    //  a.insert(0,"0");
-    //  cout << a << endl;
-    
+    RSA receptor( 1024 ); 
+    RSA emisor( receptor.e , receptor.n ); //  e  , n   
+    string cifrado = emisor.cifrado("Miguel Alberto Motta Vilca 72210866 CampoVerdeA33Sachaca") ;
+    cout <<"CIFRADO  : \n" << cifrado  << endl;
 
+    cout << "\n--------------------------------------------------------------------------------------------------------------------------------------------------------------\n" << endl;
+
+
+    string descifrado = receptor.descifrado(cifrado);
+    cout << "DESCIFRADO  : \n" << descifrado << endl;
+    
+    cout << "\n--------------------------------------------------------------------------------------------------------------------------------------------------------------\n" << endl;
+    
+    string cifradoProtocolo = emisor.cifradoConProtocolo("Miguel Alberto Motta Vilca 72210866 CampoVerdeA33Sachaca") ;
+    cout <<"CIFRADO  : \n" << cifradoProtocolo << endl;
+
+    cout << "\n--------------------------------------------------------------------------------------------------------------------------------------------------------------\n" << endl;
+
+    string descifradoProtocolo = receptor.descifradoConProtocolo(cifradoProtocolo);
+    cout << "DESCIFRADO  : \n" << descifradoProtocolo << endl;
+
+    cout << "\n--------------------------------------------------------------------------------------------------------------------------------------------------------------\n" << endl;
+    
 
     return 0;
 }
